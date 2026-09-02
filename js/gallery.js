d5f835611ce91441f1d70a4415c9370da3f33cac
maul1jm <maul1jm@gmail.com>
2026-09-02 19:48:28 +0000

    1

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,13 @@
 console.log("Guten Tag");
-console.log("Hello world!");
 
-// function upDate(previewPic){
-//  /* In this function you should 
+// function upDate(){
+
+
+
+
+}
+	
+
 //     1) change the url for the background image of the div with the id = "image" 
 //     to the source file of the preview image
     
@@ -10,9 +15,9 @@ console.log("Hello world!");
 //     to the alt text of the preview image 
 //     */
   
-// 	}
 
-// 	function unDo(){
+function unDo(preview){
+	document.getElementById("image").innerHTML = document.getElementsByClassName("preview").innerHTML = img
 //      /* In this function you should 
 //     1) Update the url for the background image of the div with the id = "image" 
 //     back to the orginal-image.  You can use the css code to see what that original URL was
@@ -21,6 +26,6 @@ console.log("Hello world!");
 //     back to the original text.  You can use the html code to see what that original text was
 //     */
 		
-// 	}
+	}
 
     
