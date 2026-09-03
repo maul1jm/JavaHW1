e5d7206e465aa4f05f2a8cac0cf9bcd7b4e1209d
maul1jm <maul1jm@gmail.com>
2026-09-03 15:51:49 +0000

    commented info for runction embedded

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,39 @@
+/*Name this external file gallery.js*/
+
+
+
+function upDate(previewPic){
+
 console.log("Guten Tag");
 
-// function upDate(){
+console.log(previewPic);
 
-
-
-
-}
-	
-
-//     1) change the url for the background image of the div with the id = "image" 
-//     to the source file of the preview image
+document.getElementById('image').innerHTML = previewPic.alt; //previewPic will get "this" variable and so previewPic.alt will point towards the content in alt.
+document.getElementById('image').style.backgroundImage = "url('" + previewPic.src +"')"; //this url will point towards the src url of this id.
+// document.getElementById('image').style.backgroundColor = 'red';
+	/* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
     
-//     2) Change the text  of the div with the id = "image" 
-//     to the alt text of the preview image 
-//     */
-  
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+ 
 
-function unDo(preview){
-	document.getElementById("image").innerHTML = document.getElementsByClassName("preview").innerHTML = img
-//      /* In this function you should 
-//     1) Update the url for the background image of the div with the id = "image" 
-//     back to the orginal-image.  You can use the css code to see what that original URL was
+	}
+ 
+	function unDo(){
+document.getElementById('image').style.backgroundImage = '';
+document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+
+
+
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
     
-//     2) Change the text  of the div with the id = "image" 
-//     back to the original text.  You can use the html code to see what that original text was
-//     */
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
 		
 	}
-
-    
